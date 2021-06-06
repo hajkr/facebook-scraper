8a6ab30889612d83c0d403df4550ee3b9e17a666
Tadej Hribar <tadej.hribar@safesize.com>
2021-06-06 10:44:55 +0200

    Only log to console and bump version to 0.1.6

--- hajkr_facebook_scraper/__main__.py
+++ hajkr_facebook_scraper/__main__.py
@@ -9,15 +9,15 @@ def enable_logging():
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s', '%m-%d-%Y %H:%M:%S')
 
-    file_handler = logging.FileHandler('logs/debug.log')
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
+    # file_handler = logging.FileHandler('logs/debug.log')
+    # file_handler.setLevel(logging.DEBUG)
+    # file_handler.setFormatter(formatter)
 
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.DEBUG)
     stream_handler.setFormatter(formatter)
 
-    logger.addHandler(file_handler)
+    # logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
 
